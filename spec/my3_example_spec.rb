253a04371988c9d5c2e2acda16ffcb6701757576
breezang1877@hotmail.com <breezang1877@hotmail.com>
2014-09-22 07:21:10 +0800

    new  Project

---
+++ spec/my3_example_spec.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+
+# describe 'My behaviour' do
+#
+#   it 'should do something' do
+#
+#     true.should == false
+#   end
+# end
+
+class Calculator
+  def add(a,b)
+    a + b
+  end
+end
+
+RSpec.describe Calculator do
+  describe '#add' do
+    it 'returns the sum of its arguments' do
+      expect(Calculator.new.add(1, 2)).to eq(3)
+    end
+  end
+end
---
+++ spec/my_example_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+
+describe 'My behaviour' do
+
+  it 'should do something' do
+
+    #true.should == false
+  end
+end
+
+
+describe Hash do
+  before do
+    @hash = Hash.new({:hello => 'world'})
+  end
+
+  it "should return a blank instance" do
+     Hash.new.should == {}
+  end
+
+  it "hash the correct information in a key" do
+    #@hash[:hello].should == 'world'
+  end
+end
